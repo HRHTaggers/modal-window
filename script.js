f1300ea5836bd898f073342685b17789b5cd1022
HRHTaggers <99132424+HRHTaggers@users.noreply.github.com>
2022-06-15 21:20:07 +0100

    Added classlist functions to create modal window

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
 'use strict';
+
+//SELECTOR VARIABLES
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+//FUNCTION VARIABLES
+const closeModal = function () {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+}
+
+const openModal = function () {
+    modal.classList.remove("hidden");
+    overlay.classList.remove("hidden");
+}
+//LOOPS
+
+for(let i = 0; i < btnsOpenModal.length; i++)
+btnsOpenModal[i].addEventListener(
+    'click', openModal);
+
+//CLICK EVENTS
+btnCloseModal.addEventListener('click', closeModal);
+
+overlay.addEventListener('click', closeModal);
+
+//KEYBOARD EVENTS
+document.addEventListener(
+    'keydown', function (event) {
+        if (event.key === "Escape" && !modal.classList.contains("hidden")) {
+            closeModal();
+        }
+    }
+)
